ae7b8b6cd7dbbccba67f59820cef67d570992a74
qulinn <akari.izumi502@gmail.com>
2023-06-04 19:37:10 +0900

    Added files.

--- study-plan/leetcode75/643-maximum-average-subarray1.py
+++ study-plan/leetcode75/643-maximum-average-subarray1.py
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 class Solution:
     def findMaxAverage(self, nums: list, k: int) -> float:
+=======
+def findMaxAverage(nums, k) -> float:
+>>>>>>> f7cda91 (Added files.)
         max_sum = curr = sum(nums[:k])
         for i in range(k, len(nums)):
             curr += nums[i] - nums[i-k]
@@ -7,9 +11,18 @@ class Solution:
                 max_sum = curr
         return max_sum / k
     
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> f7cda91 (Added files.)
 if __name__ == '__main__':
     nums = [1,12,-5,-6,50,3]
     k = 4
     expected_output = 12.75000
+<<<<<<< HEAD
     test = Solution()
     assert(test.findMaxAverage(nums,k) == expected_output)
+=======
+    assert findMaxAverage(nums,k) == expected_output
+>>>>>>> f7cda91 (Added files.)
